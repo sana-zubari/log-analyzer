62b5e0180c56360d262271c60e63d254bf4039ce
sana-zubari <sanahz1996@gmail.com>
2026-02-14 11:16:01 +0400

    Added Error Handling And Improved Reporting

--- log_analyzer.py
+++ log_analyzer.py
@@ -2,13 +2,25 @@
 
 error_count = 0
 warning_count = 0
+info_count = 0
 
-with open("sample_log.txt", "r") as file:
-    for line in file:
-        if "ERROR" in line:
-            error_count += 1
-        elif "WARNING" in line:
-            warning_count += 1
+try:
+    with open("sample_log.txt", "r") as file:
+        for line in file:
+            if "ERROR" in line:
+                error_count += 1
+            elif "WARNING" in line:
+                warning_count += 1
+            elif "INFO" in line:
+                info_count += 1
 
-print("Total Errors:", error_count)
-print("Total Warnings:", warning_count)
+    
+    print("\nLog Analysis Report")
+    print("-------------------")
+    print("Total Errors:", error_count)
+    print("Total Warnings:", warning_count)
+    print("Total Info Messages:", info_count)
+
+except FileNotFoundError:
+    print("Log file not found. Please check the file name.")
+
